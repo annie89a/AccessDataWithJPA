4e41579347a1d7fba44e90a8642f1b4af70f6b87
anisha <anisha89a@gmail.com>
2023-11-12 08:57:31 +0800

    first commit

---
+++ src/test/java/com/example/AccessDataWithJPA/AccessDataWithJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AccessDataWithJPA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessDataWithJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
